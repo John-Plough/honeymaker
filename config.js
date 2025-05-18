f7089044b1a159e78b54292558887ece64cfc167
John-Plough <110638794+John-Plough@users.noreply.github.com>
2025-05-18 21:36:03 +0000

    Deploying to gh-pages from @ John-Plough/honeymaker@68175b9bbd971655c2faf311827ff504bbf68eb7 🚀

--- config.js
+++ config.js
@@ -2,6 +2,7 @@
 const isDevelopment = window.location.hostname === "localhost";
 
 const API_BASE = isDevelopment ? "http://localhost:3000" : "https://honeymaker-api.onrender.com"; // Render deployment URL
+console.log("Current API_BASE:", API_BASE);
 
 // OAuth redirect URIs
 const OAUTH_REDIRECT_URI = isDevelopment ? "http://localhost:5173" : "https://john-plough.github.io/honeymaker/"; // Production GitHub Pages URL
